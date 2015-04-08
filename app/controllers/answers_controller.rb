74db2a9095e3c651c3625442f0acb0d33150f88b
JR <jroger2908@gmail.com>
2015-04-08 19:50:31 -0400

    added create method for answers. Fixed config/routes error

--- app/controllers/answers_controller.rb
+++ app/controllers/answers_controller.rb
@@ -1,6 +1,18 @@
 class AnswersController < ApplicationController
   
   def create
+    @question = Question.find_by(id: params[:question_id])
+    @answer = @question.answer.new(answers_params)
+
+    if @answer.save
+      redirect_to question_path(@question)
+    else
+      @questions = Question.all
+      @answers = Answer.all
+      @errors = "There was a problem answering the question"
+
+      redirect_to question_path(@question)
+    end
   end
 
   def edit

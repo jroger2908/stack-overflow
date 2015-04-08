dd448f977f251471fa2e32d8f5fc9db9b696db79
Jose Roman <jferoman@gmail.com>
2015-04-08 19:19:23 -0400

    addded the show method with views to the user controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,12 @@
 class UsersController < ApplicationController
+  
+  def show 
+    @user = User.find(params[:id])
+    @questions = @user.questions.take(5)
+    @answers = @user.answers.take(5)
+  end
+
+  def new
+   
+  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,8 @@
 class User < ActiveRecord::Base
-  validates: :name, presence: :true
-  validates: :username, presence: :true,
+  validates :name, presence: :true
+  validates :username, presence: :true,
                         uniqueness: :true
-  validates: :password, presence: :true
+  validates :password_digest, presence: :true,
                         length: { minimum: 4 }
 
   has_many :questions

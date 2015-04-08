c8ff729bf461e879ae4a638c02264a8e246f2575
Jose Roman <jferoman@gmail.com>
2015-04-08 19:33:48 -0400

    added the create method to the users controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,6 +7,24 @@ class UsersController < ApplicationController
   end
 
   def new
-   
+   @user = User.new
+  end
+
+  def create
+  	if user_params[:password] != user_params[:password_confirmation]
+      render "new"
+    end
+   user = User.create(user_params)
+   if user.save
+   	redirect_to user_path(user)
+   else
+   	render "new"
+   end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit[:name, :location, :username, :password_digest]
   end
 end

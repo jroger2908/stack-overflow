270edf26bed8d7bffd4b1f81c64e065cfaecae24
Elizabeth Blackburn <e.r.blackburn@gmail.com>
2015-04-08 19:14:32 -0400

    Changed resources :routes to resources :sessions

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
 
-  resources :routes, only [:new, :create, :destroy]
+  resources :sessions, only [:new, :create, :destroy]
 
   resources :users
 
@@ -11,7 +11,6 @@ Rails.application.routes.draw do
     end
     resources :commments, only: [:create, :destroy]
     resources :votes, only: [:create, :destroy]
-
   end
 
   # The priority is based upon order of creation: first created -> highest priority.

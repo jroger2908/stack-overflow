935c69e571a0fa50e77bc4ac98be9de58fe506ac
JR <jroger2908@gmail.com>
2015-04-09 17:40:11 -0400

    lucas fixes comments. added comment form, fixed comment controller. need to view comment on view question page

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,17 +1,17 @@
 class CommentsController < ApplicationController
+  before_action :set_commentable
 
   def new
     @comment = Comment.new
-    @question = Question.new
-    @answer = Answer.new
   end
 
   def create
-    user = User.find_by(session[:user_id])
-    Comment.create(comment_params.merge({user: user}))
-    @question = Question.find_by(id: params[:question_id])
-
-    redirect_to @question
+    @comment = @commentable.comments.new(comment_params.merge({user: current_user}))
+    if @comment.save
+      redirect_to @question
+    else
+      render :new
+    end
   end
 
   def destroy
@@ -20,6 +20,14 @@ class CommentsController < ApplicationController
     @comment.destroy
 
     redirect_to @question
+  end
+
+  protected
+
+  def set_commentable
+    @question = Question.find_by(id: params[:question_id])
+    @answer = Answer.find_by(id: params[:answer_id]) if params[:answer_id]
+    @commentable = @answer || @question
   end
 
   def comment_params

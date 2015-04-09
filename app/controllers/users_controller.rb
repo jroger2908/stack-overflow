81edc3d78a35c66ac4ab09152461d6b5f8780bbe
Elizabeth Blackburn <e.r.blackburn@gmail.com>
2015-04-09 16:50:32 -0400

    fix indentations

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,29 +1,28 @@
 class UsersController < ApplicationController
-  
-  def show 
+
+  def show
     @user = User.find(params[:id])
     @questions = @user.questions.take(5)
     @answers = @user.answers.take(5)
   end
 
   def new
-   @user = User.new
+    @user = User.new
   end
 
   def create
-  	if user_params[:password] != user_params[:password_confirmation]
+    if user_params[:password] != user_params[:password_confirmation]
       render "new"
     end
-   user = User.create(user_params)
-   if user.save
-   	redirect_to user_path(user)
-   else
-   	render "new"
-   end
+    user = User.create(user_params)
+    if user.save
+      redirect_to user_path(user)
+    else
+      render "new"
+    end
   end
 
   def destroy
-   
   end
 
   private
@@ -31,4 +30,5 @@ class UsersController < ApplicationController
   def user_params
     params.require(:user).permit[:name, :location, :username, :password_digest]
   end
+
 end

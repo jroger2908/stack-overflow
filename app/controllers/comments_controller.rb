97036f6393f466b05771c93f95b9c54e3f6241c5
JR <jroger2908@gmail.com>
2015-04-09 15:58:34 -0400

    added create method and comment_params in controller

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,12 +1,21 @@
 class CommentsController < ApplicationController
   def create
+    user = User.find_by(session[:user_id])
+    Comment.create(comment_params.merge({user: user}))
+    @question = Question.find_by(id: params[:question_id])
+    
+    redirect_to @question
   end
 
   def destroy
     @comment = Comment.find_by(id: params[:id])
     @question = Question.find_by(id: @comment.question_id)
     @comment.destroy
-    
+
     redirect_to @question
+  end
+
+  def comment_params
+    params.require(:comment).permit(:body,:commentable_id,:commentable_type)
   end
 end

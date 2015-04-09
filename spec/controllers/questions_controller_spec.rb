656f751c63ade0efd7209f9549d69703c6a5624a
JR <jroger2908@gmail.com>
2015-04-09 10:31:27 -0400

    added question controller spec tests for index and show. fixed failing test in answer model

--- spec/controllers/questions_controller_spec.rb
+++ spec/controllers/questions_controller_spec.rb
@@ -1,5 +1,28 @@
 require 'rails_helper'
 
 RSpec.describe QuestionsController, type: :controller do
+  let!(:question) {Question.create!(title: "Test title", body: "Test body", user_id: 1)}
 
+  describe "GET #index" do
+    it "assigns all questions to @questions" do
+      get :index
+      expect(assigns(:questions)).to eq([question])
+    end
+    it "renders the :index view" do
+      get :index
+      response.should render_template :index
+    end
+  end
+
+  describe "GET #show" do
+    it "assigns the requested question to @question" do
+      get :show, {id: question.to_param}
+      assigns(:question).should eq(question)
+    end
+
+    it "renders the #show view" do
+      get :show, { id: question.to_param  }
+      response.should render_template :show
+    end
+  end
 end

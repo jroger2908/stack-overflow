fdb467161be6db0b112984c1fb55ea363d73fa1f
Jose Roman <jferoman@gmail.com>
2015-04-10 12:43:36 -0400

    merge

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -1,5 +1,54 @@
 require 'rails_helper'
 
 RSpec.describe UsersController, type: :controller do
+let!(:user) {User.create!(name: 'VanVan',location: 'Cuba',username: 'aquinomas',password: 'aquinomas')}
+let!(:lionking) {User.create!(name: 'lionking',location: 'tanzania',username: 'lionking',password: 'lionking')}
+let!(:question) {Question.create!(title: "Test title", body: "Test body", user: user)}
+let!(:question_test) {Question.create!(title: "Test title", body: "Test body", user: lionking)}
+let!(:answer) {Answer.create( body: "Some text", question: question, user: user)}
+  
+  describe "GET #show" do
+
+  	it "ir should render the shoe template" do 
+      get :show, {id: user.to_param}
+      response.should render_template :show
+    end
+
+    it "assigns all user questions to @questions" do
+      get :show, {id: user.to_param}
+      expect(assigns(:questions)).to include(question)
+    end
+
+    it "assigns all a to answers to @answers" do
+      get :show, {id: user.to_param}
+      expect(assigns(:answers)).to include(answer)
+    end
+  end
+
+  describe "GET #new" do
+    it "renders the new sing up page" do 
+      get :new
+      response.should render_template :new
+    end
+  end
+
+  # describe "POST #create" do
+  #   it "renders the new vire" do 
+  #     User.stub(:new) { mock_account(:save => true) }
+	 #  post :create, :user => {name: 'tigre',
+	 #  	                      location: 'amazonas',
+	 #  	                      username: 'tigre',
+	 #  	                      password: 'tigre' }
+	 #  # expect(response).to redirect_to(assigns(:widget))
+	 #  # follow_redirect!
+	 #  # expect(response).to render_template(:show)
+	 #  # # expect(response.body).to include("Widget was successfully created.")
+  #   end
+  # end
+   
+  # describe "DELETE #destroy" do
+   
+  # end
+ 
 
 end
--- spec/models/vote_spec.rb
+++ spec/models/vote_spec.rb
@@ -3,21 +3,21 @@ require 'rails_helper'
 RSpec.describe Vote, type: :model do
   let!(:user) {User.create!(name: 'VanVan',location: 'Cuba',username: 'aquinomas',password: 'aquinomas')}
   let!(:lionking) {User.create!(name: 'lionking',location: 'tanzania',username: 'lionking',password: 'lionking')}
-  let(:question) {Question.new(title: "Fake answer", body: "Some text", user: user)}
-  let(:answer) {Answer.new( body: "Some text", question: question, user: user)}
+  let!(:question) {Question.create(title: "Fake answer", body: "Some text", user: user)}
+  let!(:answer) {Answer.create( body: "Some text", question: question, user: user)}
 
   describe "#validations" do
 
   	it "User cannot vote on own questions or answers" do
-  		question.save
-    	answer.save
-      expect{answer.votes.create(user: user)}.to_not change{Vote.count}	     
+      expect{answer.votes.create(user: user)}.to_not change{Vote.count}	 
+      expect{question.votes.create(user: user)}.to_not change{Vote.count}     
     end
 
     it "vote of valid user on questions or answers should increase vote count" do
-    	question.save
-    	answer.save
       expect{answer.votes.create(user: lionking)}.to change{Vote.count}.by(1)
-    end 
+      expect{question.votes.create(user: lionking)}.to change{Vote.count}.by(1)
+    end
+
+
   end
 end

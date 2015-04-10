db5a8aca7ea8ec59a9dedf2bb6c3c03498a45df5
Elizabeth Blackburn <e.r.blackburn@gmail.com>
2015-04-10 11:33:58 -0400

    question model tests created and passing

--- spec/factories.rb
+++ spec/factories.rb
@@ -5,4 +5,10 @@ FactoryGirl.define do
     location { Faker::Address.country }
     password { Faker::Internet.password(4, 12)}
   end
+
+  factory :question do
+    title { Faker::Lorem.characters(79)}
+    body { Faker::Lorem.paragraph(3, false, 3)}
+    user { FactoryGirl.create(:user) }
+  end
 end

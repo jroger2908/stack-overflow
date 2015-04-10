e1929922914c28d978250d91db017fc26d650e56
Elizabeth Blackburn <e.r.blackburn@gmail.com>
2015-04-10 10:55:53 -0400

    test coverage for user model, all passing

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,34 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+
+  it "has a valid factory" do
+    expect(FactoryGirl.create(:user)).to be_valid
+  end
+
+  it "is invalid without name" do
+    expect(FactoryGirl.build(:user, name: nil)).to be_invalid
+  end
+
+  it "is invalid without username" do
+    expect(FactoryGirl.build(:user, username: nil)).to be_invalid
+  end
+
+  it "is invalid without password" do
+    expect(FactoryGirl.build(:user, password: nil)).to be_invalid
+  end
+
+  it "must have a password 4 characters or longer" do
+    expect(FactoryGirl.build(:user, password: "123")).to be_invalid
+  end
+
+  it "requires a unique username" do
+    User.create(name: "Marcelina Rutherford", location: "Martinique", username: "neko1234", password: "metalhead")
+    dupe = User.create(name: "Abigail Winthrop", location: "Costa Rica", username: "neko1234", password: "babes")
+    expect(dupe).to be_invalid
+  end
+
 end
+
+
+# Password must be 4+ characters
